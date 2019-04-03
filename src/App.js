559e93eb6c995ea2d3ddcbb89666b1d6f4c9381f
crong-k <crong24601@gmail.com>
2019-04-03 15:17:02 +0900

    ---

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import ImageUploader from './components/ImageUploader';
-import InfoButton from './components/InfoButton';
+
 
 class App extends Component {
   render() {
     return (
       
-      ReactDOM.render(<InfoButton />, document.getElementById('but'));
-      ReactDOM.render(<ImageUploader />, document.getElementById('img'));
-      
+      <h>hello world </h>
     );
   }
 }
