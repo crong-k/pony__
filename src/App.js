9a3c11c0df7227015c917d9518b124c0ff3662c8
crong-k <crong24601@gmail.com>
2019-04-03 15:01:13 +0900

    ---

--- src/App.js
+++ src/App.js
@@ -3,13 +3,35 @@ import logo from './logo.svg';
 import './App.css';
 import ImageUploader from './components/ImageUploader';
 import DrawChart from './components/DrawChart';
+import { Button, Popover, PopoverHeader, PopoverBody } from 'reactstrap';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      popoverOpen: false
+    };
+  }
+
+  toggle() {
+    this.setState({
+      popoverOpen: !this.state.popoverOpen
+    });
+  }
+
   render() {
     return (
-     NaN
+      <div>
+        <Button id="Popover1" type="button">
+          Launch Popover
+        </Button>
+        <Popover placement="bottom" isOpen={this.state.popoverOpen} target="Popover1" toggle={this.toggle}>
+          <PopoverHeader>Popover Title</PopoverHeader>
+          <PopoverBody>Sed posuere consectetur est at lobortis. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.</PopoverBody>
+        </Popover>
+      </div>
     );
   }
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import ImageUploader from './components/ImageUploader';
 import DrawChart from './components/DrawChart';
+import 'bootstrap/dist/css/bootstrap.css'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
