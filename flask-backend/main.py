baaaef7ae085850b87d35432abde234b053f9301
cron <ubuntu@ip-172-31-29-149.ap-northeast-2.compute.internal>
2019-04-04 08:18:35 +0000

    ---

--- flask-backend/main.py
+++ flask-backend/main.py
@@ -5,9 +5,5 @@ app = flask.Flask("__main__")
 def my_index() :
     return flask.render_template("index.html", token="Hello Flask + React")
 
-<<<<<<< HEAD
-app.run(host='0.0.0.0')
-=======
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
->>>>>>> 092e4a3ca084ee9fe67ba6cf531c8e98a631df68
