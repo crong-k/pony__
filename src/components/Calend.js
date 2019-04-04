6bd6f7f0e9cac78a5f461f7300b6664a654ba199
crong-k <crong24601@gmail.com>
2019-04-04 09:24:09 +0900

    ---

--- src/components/Calend.js
+++ src/components/Calend.js
@@ -1,19 +1,113 @@
 import React, {Component} from 'react';
-import { DateTimePicker } from 'react-widgets';
 
-class Calend extends Component {   
-    let { DateTimePicker } = ReactWidgets; 
-    let formatter = Globalize.dateFormatter({ raw: 'MMM dd, yyyy' })
+class DatePicker extends React.Component {
+	constructor(props) {
+		super(props);
 
-    let widget = (
-        <DateTimePicker
-        format={formatter} 
-        defaultValue={new Date()}
-        time={false}
-     />
-        )
+		this.state = {
+			year: this.props.year,
+			month: this.props.month - 1,
+			day: this.props.day,
+      chosenDate: null
+		}
 
-render(widget); 
+		this.prevMonth = this.prevMonth.bind(this);
+		this.nextMonth = this.nextMonth.bind(this);
+    this.dateSetter = this.dateSetter.bind(this);
+    this.resetDate = this.resetDate.bind(this);
+	}
+  componentWillMount() {
+    this.dateSetter(this.props.day, this.props.month - 1, this.state.year);
+  }
+	prevMonth(e) {
+		e.preventDefault();
+		let newMonth = this.state.month - 1 === -1 ? 11 : this.state.month - 1;
+		let newYear = newMonth === 11 ? this.state.year - 1 : this.state.year;
+		this.setState({year: newYear, month: newMonth});
+	}
+	nextMonth(e) {
+		e.preventDefault();
+		let newMonth = this.state.month + 1 === 12 ? 0 : this.state.month + 1;
+		let newYear = newMonth === 0 ? this.state.year + 1 : this.state.year;
+		this.setState({year: newYear, month: newMonth});
+	}
+	monthConverter(index) {
+		return ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'][index];
+	}
+  dateSetter(day, month, year, e) {
+    if(e !== undefined) e.preventDefault();
+    day = day.toString().length < 2 ? '0' + day : day;
+    month += 1;
+    month = month.toString().length < 2 ? '0' + month : month;
+    let date = `${day}.${month}.${year}`;
+    this.setState({chosenDate: date});
+  }
+  resetDate(e) {
+    e.preventDefault();
+    this.setState({year: this.props.year, month: this.props.month - 1, day: this.props.day});
+    this.dateSetter(this.props.day, this.props.month - 1, this.props.year);
+  }
+	render() {
+
+		let date = new Date(this.state.year, this.state.month, 1);
+		let weekDay = date.getDay() !== 0 ? date.getDay() : 7;
+
+		date.setMonth(date.getMonth() + 1);
+		date.setDate(1);
+		date.setDate(date.getDate() - 1);
+
+		let lastDay = date.getDate();
+		// let totalCells = weekDay - 1 + lastDay <= 35 ? 35 : 42;
+		let totalCells = 42;
+
+		let calendar = [];
+		for(let i = 0; i < totalCells; i++) {
+			if(i >= weekDay - 1 && i < weekDay + lastDay - 1) {
+				calendar[i] = i - weekDay + 2;
+			} else {
+				calendar[i] = '';
+			}
+		}
+
+		let pickerYear = this.state.year;
+		let pickerMonth = this.monthConverter(this.state.month);
+
+
+		let cells = calendar.map((item,i) => {
+			if(item !== '') {
+        let chosenDateArr = this.state.chosenDate.split('.');
+				let cls = item == chosenDateArr[0] && this.state.month == chosenDateArr[1] - 1 && this.state.year == chosenDateArr[2] ? 'current' : '';
+        let day = item;
+        let month = this.state.month;
+        let year = this.state.year;
+				return (
+					<div className="cell" key={i}>
+						<a className={cls} href="#" onClick={e => this.dateSetter(day,month,year,e)}>{item}</a>
+					</div>
+				)
+			} else {
+				return <div className="cell" key={i}></div>
+			}
+		});
+
+		return (
+			<div className="datepicker-holder">
+        <div className="heading">
+				  <a className="prev" href="#" onClick={this.prevMonth}>&lt;</a>
+				  <a className="next" href="#" onClick={this.nextMonth}>&gt;</a>
+				  {pickerMonth} {pickerYear}
+        </div>
+				<div className="datepicker">
+					{cells}
+				</div>
+        <div className="chosen-date">
+          {this.state.chosenDate}
+          <a onClick={this.resetDate} href="#">Today</a>
+        </div>
+			</div>
+		);
+	}
 }
-  
-export default Calend;
+
+export default DatePicker;
+
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import ImageUploader from './components/ImageUploader';
 import InfoButton from './components/InfoButton';
-import Calend from './components/Calend';
+import DatePicker from './components/DatePicker';
 import 'bootstrap/dist/css/bootstrap.css'
 import './index.css';
 import * as serviceWorker from './serviceWorker';
@@ -11,7 +11,9 @@ import * as serviceWorker from './serviceWorker';
 ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render(<InfoButton />, document.getElementById('butt'));
 ReactDOM.render(<ImageUploader />, document.getElementById('img'));
-ReactDOM.render(<Calend />, document.getElementById('calen'));
+ReactDOM.render(<DatePicker year={2017} month={3} day={17} />,
+	document.getElementById('calen')
+);
 
 // ReactDOM.render(<DrawChart />, document.getElementById('chart'));  //At index.html 
 
