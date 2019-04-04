7926d83c104d6a3edb91eb47a22fedac0195efe5
crong-k <crong24601@gmail.com>
2019-04-04 19:07:33 +0900

    ---

---
+++ flask-backend/main.py
@@ -0,0 +1,9 @@
+import flask
+
+app = flask.Flask("__main__")
+@app.route("/")
+def my_index() :
+    return flask.render_template("index.html", token="Hello Flask + React")
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
