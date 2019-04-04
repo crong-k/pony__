092e4a3ca084ee9fe67ba6cf531c8e98a631df68
crong-k <crong24601@gmail.com>
2019-04-04 17:08:54 +0900

    ---

--- flask-backend/main.py
+++ flask-backend/main.py
@@ -6,4 +6,5 @@ app = flask.Flask("__main__")
 def my_index() :
     return flask.render_template("index.html", token="Hello Flask + React")
 
-app.run(host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
