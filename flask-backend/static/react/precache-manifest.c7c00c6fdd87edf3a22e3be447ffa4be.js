a6f6bb5528bec576e2bfc755eabb294874c87ec9
Your Name <ubuntu@ip-172-31-29-149.ap-northeast-2.compute.internal>
2019-04-04 07:15:06 +0000

    ---

---
+++ flask-backend/static/react/precache-manifest.c7c00c6fdd87edf3a22e3be447ffa4be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "53eac5d40d2daff628d2",
+    "url": "/static/react/js/runtime~main.5d7886d3.js"
+  },
+  {
+    "revision": "ef8efb342b33c1432fc0",
+    "url": "/static/react/js/main.665379e5.chunk.js"
+  },
+  {
+    "revision": "6dc9e042bc80b29e9b84",
+    "url": "/static/react/js/2.11a112a6.chunk.js"
+  },
+  {
+    "revision": "ef8efb342b33c1432fc0",
+    "url": "/static/react/css/main.2400c52d.chunk.css"
+  },
+  {
+    "revision": "6dc9e042bc80b29e9b84",
+    "url": "/static/react/css/2.84aeacdb.chunk.css"
+  },
+  {
+    "revision": "a33aeca54c5cc6a241038628f373b709",
+    "url": "/static/react/../../../../templates/index.html"
+  }
+];
