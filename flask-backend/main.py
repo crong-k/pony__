0610aa98949b9d9848f405a4c6967fb57aacce9c
crong-k <crong24601@gmail.com>
2019-04-04 20:32:31 +0900

    ---

--- flask-backend/main.py
+++ flask-backend/main.py
@@ -1,9 +1,18 @@
-import flask
+from flask import Flask, request, render_template
+import json
 
-app = flask.Flask("__main__")
+
+app = Flask("__main__")
 @app.route("/")
 def my_index() :
-    return flask.render_template("index.html", token="Hello Flask + React")
+    return render_template("index.html", token="Hello Flask + React")
+
+@app.route("/user/12345", methods=['POST'])
+def ajax_post(request):
+    json = request.get_json()  
+    print (json)
+
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
