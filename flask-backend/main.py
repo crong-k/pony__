5b56caa134c3554598b6da1e1c62449bfbf82cc4
crong-k <crong24601@gmail.com>
2019-04-05 14:57:49 +0900

    ---

--- flask-backend/main.py
+++ flask-backend/main.py
@@ -13,6 +13,10 @@ app.config['SECRET_KEY'] = 'any secret string'
 def index():
     return render_template("index.html")
 
+@app.route('/test', methods=['GET', 'POST'])
+def test():
+    vars = request.data
+    return ', '.join([str(i) for i in vars])
     
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True)
