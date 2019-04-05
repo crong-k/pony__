245be4aba399dcfb0d0d36295e9d67e4c1e5b68a
crong-k <crong24601@gmail.com>
2019-04-05 14:49:13 +0900

    ---

--- flask-backend/main.py
+++ flask-backend/main.py
@@ -8,19 +8,16 @@ csrf = CsrfProtect()
 csrf.init_app(app)
 app.config['SECRET_KEY'] = 'any secret string'
 
-@app.route('/')
+@app.route("/")
+def hello():
+    return "Welcome to Python Flask!"
+
 @app.route('/index')
 def index():
     return render_template("index.html")
 
-@app.route('/HelloWorld', methods=['GET', 'POST'])
-def HelloWorldRouted():
-    if request.method == "POST":
-        try:
-            username = str(request.json['username'])
-            return "Hello "+username+"!"
-        except Exception as e:
-            return "error: "+str(e)
+from flask import render_template
+
     
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True)
