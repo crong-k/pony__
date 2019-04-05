c2e6636d605b82a3a46d66a77120e787616e4329
crong-k <crong24601@gmail.com>
2019-04-05 15:12:13 +0900

    ---

--- flask-backend/main.py
+++ flask-backend/main.py
@@ -8,17 +8,16 @@ csrf = CsrfProtect()
 csrf.init_app(app)
 app.config['SECRET_KEY'] = 'any secret string'
 
-@app.route("/")
-def index():
-    return render_template('index.html')
+#@app.route("/")
+#def index():
+#    return render_template('index.html')
 
-@app.route("/echo", methods=["POST"])
-def echo():
-    print('echo called')
-    data = request.values.get('param', '')
-    print(data)
-    return data
-
+@app.route('/test/', methods=['GET','POST'])
+def test():
+      clicked=None
+      if request.method == "POST":
+          clicked=request.json['data']
+     return render_template(index.html')
     
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True)
