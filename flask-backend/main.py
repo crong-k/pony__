ae3b1aff55f0314b2c7561c6a0ef5f880813f189
crong-k <crong24601@gmail.com>
2019-04-05 16:41:30 +0900

    ---

--- flask-backend/main.py
+++ flask-backend/main.py
@@ -16,21 +16,12 @@ app.config['SECRET_KEY'] = 'any secret string'
 def test():
     return render_template('index.html')
 
-#@app.route('/post', methods=['GET', 'POST'])
-#def post():
-#    value = request.form['test']
-#    print(value)
-#    return value
+@app.route('/post', methods=['POST'])
+def post():
+    value = request.form['test']
+    print(value)
+    return value
 
-@app.route('/test', methods=['GET', 'POST'])
-def test():
-    if request.method=='GET':
-        return('<form action="/test" method="post"><input type="submit" value="Send" /></form>')
-
-    elif request.method=='POST':
-        return "OK this is a post method"
-    else:
-        return("ok")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True)
