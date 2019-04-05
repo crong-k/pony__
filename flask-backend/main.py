406b2c9b6875d3d7407f544148b9cd0a73804f15
crong-k <crong24601@gmail.com>
2019-04-05 13:58:42 +0900

    ---

--- flask-backend/main.py
+++ flask-backend/main.py
@@ -12,15 +12,14 @@ app.config['SECRET_KEY'] = 'any secret string'
 def index():
     return render_template("index.html")
 
-@app.route("/ajax_post", methods=['POST'])
-def ajax_post():
-    data = request.form['keyword']
+@app.route("/process/", methods=['POST'])
+def process():
+    data = request.form.get('keyword')
     print(data)
-    msg = "Flask got successfully ajax request!"
-    response = make_response(json.dumps(msg))
-    response.status_code = 200
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    return response
-
+    #msg = "Flask got successfully ajax request!"
+    #response = make_response(json.dumps(msg))
+    #response.status_code = 200
+    #response.headers['Access-Control-Allow-Origin'] = '*'
+    #return response
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True)
