bf679e93e9b8f15975149fc18d002b5be7dd1161
crong-k <crong24601@gmail.com>
2019-04-05 12:32:21 +0900

    ---

--- flask-backend/main.py
+++ flask-backend/main.py
@@ -3,10 +3,17 @@ from flask_wtf.csrf import CsrfProtect
 import json
 
 app = Flask(__name__, static_folder="./static", template_folder="./templates")
+csrf = CSRFProtect()
+csrf.init_app(app)
+
 
 @app.route("/")
 def index():
+    #csrf_token = "12344"
+    
     return render_template("index.html")
+    #return render_template("index.html", csrf_token=csrf_token)
+
 
 @app.route("/ajax_post", methods=['POST'])
 def ajax_post():
