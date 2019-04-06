c38d802f730b54aafd766c5a9223855550e3e366
crong-k <crong24601@gmail.com>
2019-04-06 20:54:56 +0900

    ---

--- amore_app/src/components/PredModel.js
+++ amore_app/src/components/PredModel.js
@@ -2,16 +2,46 @@ import React, {Component} from 'react';
 import * as tf from '@tensorflow/tfjs';
 import DrawChart from './DrawChart';
 
-class PredModel extends Component {
-    static defaultProps ={
-        predResult: [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-        InputImage: ''
-      }  
-    render(){
+class ImageUploader extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            image: '', 
+            imagePreviewUrl: '',
+            imgData: '',
+            predResult: [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1], //for connect 
+        };
+    }
+    handleImageChange(e) {
+        let reader = new FileReader();
+        let image = e.target.files[0];     
+        console.log('check 1')
+        reader.onloadend = () => {
+            this.setState({
+                image: image,
+                imagePreviewUrl: reader.result
+            });
+        }
+        reader.readAsDataURL(image);
+    }
 
-        const promise = tf.loadLayersModel('tea_model02/model.json');
-        promise.then((gt_model) => {
-        const catEl = this.props.InputImage;
+    getImageData() {
+        let imgData = this.refs.noData;
+        console.log('check 2')
+        //this.predictFromImage(imgData);
+        this.setState({
+            imgData: this.refs.noData,   
+        });
+        console.log('img_info',imgData)
+        this.predictFromImage(imgData);
+        return imgData    }   
+  
+     
+predictFromImage(imgData){
+    console.log('check 3')
+    const promise = tf.loadLayersModel('tea_model02/model.json');
+    promise.then((gt_model) => {
+        const catEl = imgData;
         const img = tf.browser.fromPixels(catEl).toFloat();
         const offset = tf.scalar(225.0);
         const normalized = img.div(offset);
@@ -26,19 +56,34 @@ class PredModel extends Component {
 
     }).catch(function (err) {
         console.error(err); // Error 출력
-    });   
-    
-       let {predResult} = this.sate
+    });    
+}
+    render(){
+        console.log('check 4 - render')
+        let {imagePreviewUrl} = this.state;
+        let $imagePreview = null;
+        const noImageUrl = "no-image.png";
+        let {predResult} = this.state;   //this.state;
         
+        if (imagePreviewUrl){
+            $imagePreview = (<img id="input_img" ref="noData" src={imagePreviewUrl} />);
+    
+        } else {
+            $imagePreview = (<img id="no_img" ref="noData" src={noImageUrl}/>);
+    
+        }
         return(
-          
-            <div>             
-                
+            <div className="imagePreview">
+                <input className="imageInput" type="file" onChange={(e)=> {this.handleImageChange(e); 
+                 this.getImageData();}}/>
+            <div>
+                {$imagePreview}
+                {console.log("before_render: ",predResult)}
                 <DrawChart yFromParent={predResult}/>   
             </div>
-         
-       
+            <div></div>
+        </div>
     );
 }
 }
-export default PredModel;
+export default ImageUploader;
